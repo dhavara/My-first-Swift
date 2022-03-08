18af2cc9e719bd42822b46f2ac54616637499ccc
dhavara <>
2022-03-08 01:46:35 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,93 @@
+
+
+
+
+// var largestNumber: Int
+
+// let number1 = 40
+// let number2 = 5
+
+// if number1 > number2 {
+//     largestNumber = number1
+// }else{
+//     largestNumber = number2
+// }
+
+//ternary op
+// largestNumber = (number1 > number2) ? number1 : number2
+
+// //String literal
+// print("Largest number is: \(largestNumber)")
+
+
+// let letter: Character = "u"
+
+// switch letter {
+// case "a", "i", "u", "e", "o":
+//     print("The letter is a vowel")
+// default:
+//     print("the letter is consonant")
+// }
+
+
+// let distance: Int = 250
+
+// switch distance {
+// case 0...9:
+//     print("Your destination is close")
+// case 10...99:
+//     print("Your destination is not that far")
+// case 100...999:
+//     print("Your destinantion is far from here")
+// default:
+//     print("Where are you going?")
+// }
+
+
+
+// var numberOfWheels = 3
+
+// switch numberOfWheels {
+// case 0:
+//     print("Missing Wheels")
+// case 1:
+//     print("Unicycle")
+// case 2:
+//     print("Bicycle")
+// case 3:
+//     print("Reliant Robin")
+// case 4:
+//     print("Car")
+// default:
+//     print("That's a lot of wheels")
+// }
+
+
+
+// var temperature = 100
+
+// if temperature >= 100 {
+//     print ("the water is boiling")
+// } else{
+//     print("the water is not boiling")
+// }
+
+
+
+// var temperature = 75
+
+// if temperature >= 65 && temperature <= 75 {
+//     print ("Temperature is okay")
+// } else if temperature < 65{
+//     print("it is too cold")
+// }else{
+//   print("it is too hot")
+// }
+
+
+
+// var isRain = false
+
+// if !isRain{
+//     print("Bring your umbrella")
+// }
